aac22f60e2b7152faf65c8fe07728895ead10312
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-03-19 15:56:35 +0330

    create Timer button and create Timer def

--- Digital clock.py
+++ Digital clock.py
@@ -1,6 +1,14 @@
 # import library(s)
 from tkinter import *
 import time
+# Create def
+def digital_clock(): 
+    time_live = time.strftime("%H:%M:%S")
+    label.config(text=time_live) 
+    label.after(200, digital_clock)
+digital_clock()
+def Timer():
+    pass
 # Create valu
 DigitalClockWindow = Tk()
 text_font= ("Boulder", 50, 'bold')
@@ -9,12 +17,7 @@ foreground= "white"
 border_width = 65
 label = Label(DigitalClockWindow, font=text_font, bg=background, fg=foreground, bd=border_width) 
 label.grid(row=0, column=1)
-# Create def
-def digital_clock(): 
-    time_live = time.strftime("%H:%M:%S")
-    label.config(text=time_live) 
-    label.after(200, digital_clock)
-digital_clock()
+Button(DigitalClockWindow, text='Timer', command=Timer)
 # Window ability(s)
 DigitalClockWindow.title("Digital Clock") 
 DigitalClockWindow.geometry("390x200") 
