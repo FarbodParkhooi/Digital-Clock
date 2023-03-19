020428f6665c0eacb759060c7d45f3569827e9a1
Farbod Parkhooi <farbod.p1390@gmail.com>
2023-03-19 15:53:20 +0330

    end create clock

--- Digital clock.py
+++ Digital clock.py
@@ -1,10 +1,22 @@
-# Digital clock softwar
-# import library
+# import library(s)
 from tkinter import *
-import time as tm
-# Create window
-Window = Tk()
-# Window ability
-Window.title('Digital clock')
-Window.resizable(False,False)
-Window.mainloop()
+import time
+# Create valu
+DigitalClockWindow = Tk()
+text_font= ("Boulder", 50, 'bold')
+background = "black"
+foreground= "white"
+border_width = 65
+label = Label(DigitalClockWindow, font=text_font, bg=background, fg=foreground, bd=border_width) 
+label.grid(row=0, column=1)
+# Create def
+def digital_clock(): 
+    time_live = time.strftime("%H:%M:%S")
+    label.config(text=time_live) 
+    label.after(200, digital_clock)
+digital_clock()
+# Window ability(s)
+DigitalClockWindow.title("Digital Clock") 
+DigitalClockWindow.geometry("390x200") 
+DigitalClockWindow.resizable(False,False)
+DigitalClockWindow.mainloop()
